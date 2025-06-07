db7967151e89ef783b5288e0f99e8ba83ce0bf75
saimoh03 <saimoh03871@stud.noroff.no>
2025-06-08 00:25:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Load navbar.html into #navbar-placeholder
-window.addEventListener("DOMContentLoaded", () => {
-  fetch("navbar.html")
-    .then((res) => res.text())
-    .then((html) => {
-      document.getElementById("navbar-placeholder").innerHTML = html;
-    });
-});
+// window.addEventListener("DOMContentLoaded", () => {
+//   fetch("navbar.html")
+//     .then((res) => res.text())
+//     .then((html) => {
+//       document.getElementById("navbar-placeholder").innerHTML = html;
+//     });
+// });
